8ddf30932ec548ac49510aa343ef4721ca024bf4
kelvin.tryanto <kelvin.tryanto@gmail.com>
2018-12-06 23:18:20 +0700

    My First Commit

---
+++ js/script.js
@@ -0,0 +1,98 @@
+var app = angular.module('myApp', ["ngRoute"]);
+
+app.config(function($routeProvider,$locationProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl: 'view/login.html',
+	})
+	.when('/home',{
+		templateUrl: 'view/home.html',
+		controller: 'homeCtrl'
+	})
+	.when('/aboutme',{
+		templateUrl: 'view/aboutme.html',
+		controller: 'aboutmeCtrl'
+	})
+
+	$locationProvider.html5Mode({
+		enabled: true,
+		requireBase: false
+	})
+})
+
+app.controller('homeCtrl', ['$scope', function($scope){
+	var data = "{}";
+
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+
+	xhr.addEventListener("readystatechange", function () {
+		if (this.readyState === this.DONE) {
+			console.log(this.responseText);
+		}
+	});
+
+	xhr.open("GET", "https://api.themoviedb.org/3/movie/now_playing?page=1&language=en-US&api_key=0ef9f010abd55b80f8d07f2cf200ea9a");
+
+	xhr.send(data);
+}])
+
+app.controller('aboutmeCtrl', ['$scope', function($scope){
+
+}])
+
+app.controller('myController', function($scope,$location,$rootScope){
+	$scope.user={'username':'','password':''};
+	//----- Users json
+	
+	$scope.showError = false; // set Error flag
+	$scope.showSuccess = false; // set Success Flag
+
+	var validUsers= {'username':'user', 'password':'userpti'};
+	localStorage.setItem('UserID', JSON.stringify(validUsers));
+
+	//------------ini untuk mengambil item UserID-----------------//
+	// var retrievedUser = JSON.parse(localStorage.getItem('UserID'));
+	// console.log(retrievedUser);
+	
+
+	// ------- Authenticate function
+	$scope.authenticate = function (){
+		var flag= false;
+		localStorage.setItem('flagLocal',JSON.stringify(flag));
+		
+		for(var i in validUsers){ // loop on users array
+			if($scope.user.username == JSON.parse(localStorage.getItem('UserID')).username && $scope.user.password == JSON.parse(localStorage.getItem('UserID')).password){
+				$location.path('/home')
+				flag = true;
+				localStorage.setItem('flagLocal',JSON.stringify(flag));
+				$rootScope.loggedIn = true;
+				console.log('loggedIn');
+				break;
+			}
+			else{
+				console.log('loggedOut');
+				flag = false;
+				localStorage.setItem('flagLocal',JSON.stringify(flag));
+				break;
+			}				
+		}
+
+		//-------- set error or success flags
+		if(JSON.parse(localStorage.getItem('flagLocal'))){
+			$scope.showError = false;
+			$scope.showSuccess = true;
+		}
+		else{
+			$scope.showError = true;
+			$scope.showSuccess = false;
+		}
+	}
+
+	$scope.logout = function(){
+		$rootScope.loggedIn= false
+		alert('Log Out Success')
+		$location.path('/')		
+	}
+
+});
